02a57476b0a458ff243e5e3ce4fea3c9c72e2bb2
Sudeep-cv <basicsinjava@gmail.com>
2019-11-26 08:29:58 +0000

    if condition test

--- src/main/java/com/sudeep/App.java
+++ src/main/java/com/sudeep/App.java
@@ -17,7 +17,7 @@ public class App {
         FileWriter writer = new FileWriter(new File("./src/conf.tpl.output.txt"));
         context.put("NAME", "Sudeep");
         context.put("COMPANY", "Vodafone");
-        context.put("CONDITION",false);
+        context.put("CONDITION",true);
         template.merge(context, writer);
         writer.flush();
         writer.close();
